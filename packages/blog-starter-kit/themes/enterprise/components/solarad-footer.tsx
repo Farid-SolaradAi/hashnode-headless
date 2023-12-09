bc0d3c6ec509b83b668371fce8f6746ac88d9ba1
Farid-SolaradAi <farid.ahmed@solarad.ai>
2023-12-09 17:55:45 +0530

    fix link error

--- packages/blog-starter-kit/themes/enterprise/components/solarad-footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/solarad-footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const SolaradFooter: React.FC = () => {
   const currentYear = new Date().getFullYear();
@@ -20,9 +21,9 @@ const SolaradFooter: React.FC = () => {
           <div>
             <h5 className="text-[#fcba03] font-bold">Products</h5>
             <div className="flex flex-col">
-              <a className="text-white mb-2" href="/forecast">Forecast</a>
-              <a className="text-white mb-2" href="http://app.solarad.ai">Weather Intelligence</a>
-              <a className="text-white mb-2" href="http://app.solarad.ai">Weather APIs for Solar</a>
+              <Link href="/forecast" passHref><span className="text-white mb-2 cursor-pointer">Forecast</span></Link>
+              <a href="http://app.solarad.ai" className="text-white mb-2">Weather Intelligence</a>
+              <a href="http://app.solarad.ai" className="text-white mb-2">Weather APIs for Solar</a>
             </div>
           </div>
 
@@ -30,9 +31,9 @@ const SolaradFooter: React.FC = () => {
           <div>
             <h5 className="text-[#fcba03] font-bold">Resources</h5>
             <div className="flex flex-col">
-              <a className="text-white mb-2" href="/company">Company</a>
-              <a className="text-white mb-2" href="/pricing">Pricing</a>
-              <a className="text-white mb-2" href="https://solarad.notion.site">Blog</a>
+              <Link href="/company" passHref><span className="text-white mb-2 cursor-pointer">Company</span></Link>
+              <Link href="/pricing" passHref><span className="text-white mb-2 cursor-pointer">Pricing</span></Link>
+              <a href="https://solarad.notion.site" className="text-white mb-2">Blog</a>
             </div>
           </div>
 
@@ -40,11 +41,8 @@ const SolaradFooter: React.FC = () => {
           <div>
             <h5 className="text-[#fcba03] font-bold">Get In Touch</h5>
             <div className="flex flex-col">
-              <a className="text-white mb-2" href="mailto:founders@solarad.ai">founders@solarad.ai</a>
-              {/* LinkedIn Button */}
-              <a href="https://www.linkedin.com/company/solarad-ai/" className="text-[#fcba03] hover:text-white">
-                <i className="fab fa-linkedin"></i> Follow us on LinkedIn
-              </a>
+              <a href="mailto:founders@solarad.ai" className="text-white mb-2">founders@solarad.ai</a>
+              <a href="https://www.linkedin.com/company/solarad-ai/" className="text-[#fcba03] hover:text-white">Follow us on LinkedIn</a>
             </div>
           </div>
         </div>
@@ -57,8 +55,8 @@ const SolaradFooter: React.FC = () => {
             <div>
               <h5 className="text-white font-bold">Solarad AI, USA</h5>
               <div className="flex flex-col">
-                <a className="text-white mb-1" href="/company">226 Haight St., San Francisco, CA 94102</a>
-                <a className="text-white mb-1" href="/company">+1 628 800 0056</a>
+                <Link href="/company" passHref><span className="text-white mb-1 cursor-pointer">226 Haight St., San Francisco, CA 94102</span></Link>
+                <Link href="/company" passHref><span className="text-white mb-1 cursor-pointer">+1 628 800 0056</span></Link>
               </div>
             </div>
 
@@ -66,8 +64,8 @@ const SolaradFooter: React.FC = () => {
             <div>
               <h5 className="text-white font-bold">Solarad AI, India</h5>
               <div className="flex flex-col">
-                <a className="text-white mb-2" href="/company">145, Sector 44, Gurugram, Haryana 122003</a>
-                <a className="text-white mb-1" href="/company">+91 97951 19246</a>
+                <Link href="/company" passHref><span className="text-white mb-2 cursor-pointer">145, Sector 44, Gurugram, Haryana 122003</span></Link>
+                <Link href="/company" passHref><span className="text-white mb-1 cursor-pointer">+91 97951 19246</span></Link>
               </div>
             </div>
           </div>
@@ -76,11 +74,11 @@ const SolaradFooter: React.FC = () => {
         {/* Footer Bottom Row */}
         <div className="flex flex-wrap justify-between items-center mt-5 border-t border-gray-700 pt-3">
           <div className="text-sm text-white">
-            © {currentYear} <a href="/" className='text-[#fcba03]'>Solarad.ai</a> All rights reserved.
+            © {currentYear} <Link href="/" passHref><span className='text-[#fcba03] cursor-pointer'>Solarad.ai</span></Link> All rights reserved.
           </div>
           <div className="flex space-x-4">
-            <a href="#" className="text-gray-400 hover:text-white">Newsletter</a>
-            <a href="#" className="text-gray-400 hover:text-white">Privacy Policy</a>
+            <Link href="#" passHref><span className="text-gray-400 hover:text-white cursor-pointer">Newsletter</span></Link>
+            <Link href="#" passHref><span className="text-gray-400 hover:text-white cursor-pointer">Privacy Policy</span></Link>
           </div>
         </div>
       </div>
