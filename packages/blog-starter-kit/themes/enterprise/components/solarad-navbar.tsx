164ff85ce89db682cb0dceb49c1d001506b6762f
Farid-SolaradAi <farid.ahmed@solarad.ai>
2023-12-11 14:37:48 +0530

    redirect newsletter to blog.solarad.ai

--- packages/blog-starter-kit/themes/enterprise/components/solarad-navbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/solarad-navbar.tsx
@@ -21,7 +21,7 @@ const SolaradNavbar: React.FC = () => {
             <div className="hidden md:flex items-center space-x-9 mr-9">
                 {/* Products Dropdown */}
                 <div className="relative" onMouseEnter={toggleProductsDropdown} onMouseLeave={toggleProductsDropdown}>
-                        <span className="text-white hover:text-gray-400 transition-colors">Products</span>
+                        <span className="text-white hover:text-gray-400 transition-colors hover:cursor-pointer">Products</span>
                     {isProductsDropdownOpen && (
                         <div className="absolute pt-2 w-48 rounded-md shadow-lg top-full">
                             <div className="px-4 py-1 text-sm bg-white box-border rounded-t-md"></div>
@@ -48,16 +48,16 @@ const SolaradNavbar: React.FC = () => {
 
                 {/* Resources Dropdown */}
                 <div className="relative mr-9" onMouseEnter={toggleResourcesDropdown} onMouseLeave={toggleResourcesDropdown}>
-                    <Link href="/resources" legacyBehavior>
-                        <a className="text-white hover:text-gray-400 transition-colors">Resources</a>
-                    </Link>
+                    {/* <Link href="/resources" legacyBehavior> */}
+                        <span className="text-white hover:text-gray-400 transition-colors hover:cursor-pointer">Resources</span>
+                    {/* </Link> */}
                     {isResourcesDropdownOpen && (
                         <div className="absolute pt-2 w-48 rounded-md shadow-lg flex flex-col justify-end">
                             <div className="px-4 py-1 text-sm bg-white box-border rounded-t-md"></div>
                             <Link href="https://blog.solarad.ai" legacyBehavior>
                                 <a className="block px-4 py-3 text-sm text-gray-700 bg-white hover:bg-gray-100">Blog</a>
                             </Link>
-                            <Link href="https://solarad.notion.site/c13f74cd948f4531b1b19de3a736f97c?v=e82f893a2d6647958dbe815bd2583d19" legacyBehavior>
+                            <Link href="https://blog.solarad.ai" legacyBehavior>
                                 <a className="block px-4 py-3 text-sm text-gray-700 bg-white hover:bg-gray-100">Newsletter</a>
                             </Link>
                             <Link href="https://solarad.ai/resource-map" legacyBehavior>
